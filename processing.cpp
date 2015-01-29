80e53a5d51c478e90023716b877afeac030ba609
Anthony Correia <anthony.correia71@gmail.com>
2015-01-29 15:31:26 +0100

    Re-organized the code and added new methods for better code seperation. Beware: the main is not functionnal.

--- processing.cpp
+++ processing.cpp
@@ -9,32 +9,20 @@
 using namespace std;
 using namespace cv;
 
-void processImage(Mat &imgOriginal)
+void processImage(Mat &img)
 {
-    Mat imgHSV;
-
-    cvtColor(imgOriginal, imgHSV, COLOR_RGB2HSV); //Convert the captured frame from BGR to HSV
-
-    Mat imgThresholded;
-
-    inRange(imgHSV, Scalar(0, 0, 240), Scalar(179, 20, 255), imgThresholded); //Threshold the image
-    imwrite("/home/pi/ram/thresh.jpg", imgThresholded);
-
-    vector<vector <Point> > contours;
-
-    findContours(imgThresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-
-    cout << "Contours foud: " << contours.size() << endl;
+    Mat imgOriginal(img);
     
-    vector<Moments> vec_moments(contours.size());
+    prepareImage(img);
     
-    computeMoments(vec_moments, contours);
-    
+    vector<Moments> vec_moments;
+    extractMoments(vec_moments, img);
+
     uint ind_robot = 0;
-    
+
     vector<Moments>::iterator it = vec_moments.begin();
 
-    while(!vec_moments.empty())
+    while (!vec_moments.empty())
     {
         Moments m = *it;
         double dM01 = m.m01;
@@ -47,83 +35,109 @@ void processImage(Mat &imgOriginal)
             posX = dM10 / dArea;
             posY = dM01 / dArea;
             Robot* r = Robot::getRobot(ind_robot);
-            if(!r->tryPosition(posX, posY))
+            if (!r->tryPosition(posX, posY))
             {
                 it++;
-            }
-            else
+            } else
             {
                 vec_moments.erase(it);
-                if(++ind_robot == Robot::numberOfRobots())
+                if (++ind_robot == Robot::numberOfRobots())
                     break;
             }
-        }
-        else
+        } else
         {
             vec_moments.erase(it);
         }
     }
     send(Robot::robotsToJSON());
-    drawContours(imgOriginal, contours, -1, Scalar(255, 0, 0));
-    imwrite("/home/pi/ram/contours.jpg", imgOriginal);
-    int a;
-    //cin >> a;
+}
+
+void prepareImage(Mat& imgOriginal)
+{
+    Mat imgHSV;
+
+    cvtColor(imgOriginal, imgHSV, COLOR_RGB2HSV); //Convert the captured frame from BGR to HSV
+
+    Mat imgThresholded;
+
+    inRange(imgHSV, Scalar(0, 0, 240), Scalar(179, 20, 255), imgThresholded); //Threshold the image
+    imwrite("/home/pi/ram/thresh.jpg", imgThresholded);
+    
+    imgOriginal = imgThresholded;
 }
 
 void captureLoop()
 {
     int capturedFrames = 0;
+    Mat imgOriginal;
     while (true)
     {
-        if (!captureSignal())
-        {
-            return;
-        }
-
+        captureFrame(imgOriginal);
         capturedFrames++;
-
-        //Waiting for the image to be saved.
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-
-        cout << "Trying to open " << CAPTURE_PATH << ", frame number " << capturedFrames << "\n";
-        Mat imgOriginal;
-
-        int failures = 0;
-        do
-        {
-            imgOriginal = imread(CAPTURE_PATH);
-            if (imgOriginal.data == NULL)
-            {
-                cerr << "Error while reading " << CAPTURE_PATH << ".\n" << endl;
-                failures++;
-                if (failures > 1000)
-                {
-                    cout << "More than 10 failures. Exiting...\n";
-                    exit(1);
-                }
-                cout << "Trying again.\n";
-            } else
-            {
-                //Image opend successfully
-                cout << "Frame number " << capturedFrames << " opened successfully.\n";
-                break;
-            }
-
-        } while (true);
-
         processImage(imgOriginal);
     }
 }
 
+bool captureFrame(Mat& imgOriginal)
+{
+    if (!captureSignal())
+    {
+        return false;
+    }
+
+    //Waiting for the image to be saved.
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    int failures = 0;
+    do
+    {
+        imgOriginal = imread(CAPTURE_PATH);
+        if (imgOriginal.data == NULL)
+        {
+            cerr << "Error while reading " << CAPTURE_PATH << ".\n" << endl;
+            failures++;
+            if (failures > MAX_FAILURES)
+            {
+                cout << "More than " << MAX_FAILURES << "  failures. Exiting...\n";
+                exit(1);
+            }
+            cout << "Trying again.\n";
+        } else
+        {
+            //Image opend successfully
+            cout << "Frame opened successfully.\n";
+            break;
+        }
+
+    } while (true);
+    return true;
+}
+
+void extractMoments(vector<Moments>& vec_moments, Mat& img)
+{
+    vector<vector <Point> > contours;
+
+    findContours(img, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    
+    drawContours(img, contours, -1, Scalar(255, 0, 0));
+    imwrite("/home/pi/ram/contours.jpg", img);
+
+    cout << "Contours foud: " << contours.size() << endl;
+    
+    vec_moments.clear();
+    vec_moments.reserve(contours.size());
+
+    computeMoments(vec_moments, contours);
+}
+
 vector<Moments>& computeMoments(vector<Moments>& vec_moments, vector<vector<Point>> vectors)
 {
-    for(vector<Point> points : vectors)
+    for (vector<Point> points : vectors)
     {
         vec_moments.push_back(moments(points));
     }
     sort(vec_moments.begin(), vec_moments.end(), sortMoments);
-    
+
     return vec_moments;
 }
 
--- processing.h
+++ processing.h
@@ -6,16 +6,25 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 
+#define MAX_FAILURES 500
+
 /**
- * @brief Processes the image and extracts the coordinates.
+ * @brief Processes the image and update the robots' coordinates.
  * @param img
  */
 void processImage(cv::Mat &img);
 
 /**
- * 
+ * Capture and run image processing until tracking mode is on.
  */
 void captureLoop();
+
+/**
+ * Capture one frame and stores it in parameter.
+ * @param img
+ * @return 
+ */
+bool captureFrame(cv::Mat& img);
 
 /**
  * Used to sort a vector of Moments.
@@ -25,6 +34,27 @@ void captureLoop();
  */
 bool sortMoments(const cv::Moments& m1, const cv::Moments m2);
 
+/**
+ * Find contours and computes moments.
+ * @param vec_moments
+ * @param img
+ */
+void extractMoments(std::vector<cv::Moments>& vec_moments, cv::Mat& img);
+
+/**
+ * Compute moments of passed contours.
+ * @param vec_moments
+ * @param vectors
+ * @return 
+ */
 std::vector<cv::Moments>& computeMoments(std::vector<cv::Moments>& vec_moments, std::vector<std::vector<cv::Point>> vectors);
+
+/**
+ * Prepares image for coordinates extraction.
+ * @param imgOriginal
+ * @return The thresholded image.
+ * Change image to HSV and apply threshold.
+ */
+void prepareImage(cv::Mat& imgOriginal);
 #endif	/* PROCESSING_H */
 
